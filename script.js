9cfa905b2a5770d87797d0b03ed315e2a4b57549
arqam_hussain <arqamtahir125@gmail.com>
2025-07-23 22:59:49 +0500

    work

--- script.js
+++
@@ -1,7 +0,0 @@
-const btn = document.getElementById('colorBtn');
-
-btn.addEventListener('click', () => {
-  const colors = ['#FF6B6B', '#6BCB77', '#4D96FF', '#F7B801', '#9D4EDD'];
-  const randomColor = colors[Math.floor(Math.random() * colors.length)];
-  document.body.style.backgroundColor = randomColor;
-});
